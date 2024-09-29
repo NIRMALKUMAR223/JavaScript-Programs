9ff27b90b0d2653c019ce8c9e1148aec0ed8a13b
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-09-29 22:14:56 +0530

    greatest of three number

---
+++ interview questions/GreatestOfThreeNumber.js
@@ -0,0 +1,12 @@
+// greatest of thre number
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 30;
+
+console.log( (number1>number2 && number1>number3)? 
+    number1: (number2>number3) ?
+    number2 : number3
+);
+
+// using trinary operator
