fc501f4538bf8e0e814c0fea67ca1ff97426b51d
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-09-29 19:32:24 +0530

    1<2<3

---
+++ Program1.js
@@ -0,0 +1,6 @@
+// left to right approch so first it will solve 1<2 
+// as a result true in js true means 1 and false means 0
+// 1 is compare with 3 1<3 this is also true 
+
+console.log(1<2<3);
+
