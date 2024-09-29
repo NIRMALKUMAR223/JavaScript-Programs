4841b22f11a24c7e6c0f1f7c940a7f6674efba16
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-09-29 20:02:32 +0530

    greatest of two numbers

---
+++ interview questions/GreatestOfTwoNumbers.js
@@ -0,0 +1,12 @@
+// greatest of two numbers 
+const number1 = 10;
+const number2 = 20;
+
+if(number1>number2){
+    console.log(number1+' is greater'); // + also work
+}else{
+    console.log(number2,' is greater'); // , also work    
+}
+
+// using ternary operator
+// console.log(number1>number2 ? number1 : number2) 
