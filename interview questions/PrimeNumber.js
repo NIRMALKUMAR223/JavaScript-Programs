e5223945c1134fc84a110709771604def5800744
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-09-30 21:20:02 +0530

    Prime Number

--- interview questions/PrimeNumber.js
+++ interview questions/PrimeNumber.js
@@ -1,6 +1,11 @@
 // Prime Number
 
 function isPrime(number){
+    
+    if(number<=1){
+        return false;
+    }
+
     for (let i = 2; i<=number/2; i++){
         if (number%i==0){
             return false;
