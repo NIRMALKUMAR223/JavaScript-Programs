8b60286ad8551450be587775f5d28de0cff5bd01
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-05 00:37:53 +0530

    Occurence in String

--- interview questions/OccurenceString.js
+++ interview questions/OccurenceString.js
@@ -2,17 +2,23 @@
 
 const str = 'helloworld';
 const counted = new Set();
+
 for (let i=0; i<str.length; i++){
     let count = 0;
     for (let j=0; j<str.length; j++){
+        
+        // it return false we intensely make it true 
+        // if it is true that element is already counted  so no need to count it again 
         if(!counted.has(str.charAt(i))){
             if (str.charAt(i)==str.charAt(j)){
                 count++;
             }
         }
     }
+
     if(count!=0){
         console.log(str.charAt(i)+" : "+ count);
         counted.add(str.charAt(i));
     }
+    
 }
