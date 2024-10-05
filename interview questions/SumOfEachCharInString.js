e5c79ba86745e90f9752667fb8c0477160eb873c
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-05 11:40:32 +0530

    Sum Of EAch Char In String

---
+++ interview questions/SumOfEachCharInString.js
@@ -0,0 +1,15 @@
+// sum of each char in a string
+
+const letter = ['w','a','b','h','a','y','z','a'];
+const count = {};
+
+letter.forEach((item)=>{
+    if (count[item]){
+        count[item]+=1;
+    }
+    else{
+        count[item]=1;
+    }
+})
+
+console.log(count);
