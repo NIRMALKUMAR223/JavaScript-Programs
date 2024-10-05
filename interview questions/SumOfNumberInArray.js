dd7d71cb93280248df89e01ba440767e29c2d03a
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-05 11:37:40 +0530

    Sum Of Array

--- interview questions/SumOfNumberInArray.js
+++ interview questions/SumOfNumberInArray.js
@@ -1,11 +1,16 @@
 // sum of number in an array
 
 const arr = [1,2,3,4,5,6,7,8,9,10];
-
 let count =0;
-
 for (let i of arr){
     count+=i; 
 }
-
 console.log(count);
+
+
+// short method
+// var sum = 0;
+// arr.forEach((item)=>{
+//     sum+=item;
+// });
+// console.log(sum);
