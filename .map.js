432342cc63290949cc3769ccf810ed8baed8cbb6
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-05 12:54:32 +0530

    map

---
+++ .map.js
@@ -0,0 +1,22 @@
+// map
+
+const obj = [
+    {
+        name : 'iphone',
+        price : 79999,
+        total : 4
+    },
+    {
+        name : 'dell',
+        price : 800000,
+        total : 2
+    },
+]
+
+const totalPriceOfEachItem = obj.map(( item, index, arr)=>{
+    return item.price * item.total;
+});
+
+console.log(totalPriceOfEachItem);
+
+// map is used to modify the existing data it return new modified data we have to store it
