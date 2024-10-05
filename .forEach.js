3327db4881e48c6be30350203a81cb5f34f8cad2
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-05 12:34:58 +0530

    forEach

---
+++ .forEach.js
@@ -0,0 +1,9 @@
+// forEach
+
+const number = [1,2,3,4,5,6,7,8,9,10];
+
+number.forEach(( item, index, number)=>{
+    console.log('number['+index+']'+" = "+item);
+})
+
+// forEach accept three parameter first parameter is value / item and next one is index and last one is array
