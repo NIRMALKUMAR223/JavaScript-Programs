f603c15c1d428f148a2e0fa1a8551ba9bd6cd675
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-06 18:11:34 +0530

    odd or even

---
+++ Programiz100/OddOrEven.js
@@ -0,0 +1,28 @@
+// odd or even
+
+const number = 6;
+
+console.log(
+    ( number % 2 ===0 )?
+    'Even' : 'Odd'
+);
+
+
+
+// using if - else
+// if ( number % 2 ===0 ){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+
+
+
+
+
+
+
+
+
