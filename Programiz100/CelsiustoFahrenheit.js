64ec1c1c1cde731c7cf50869b6084290eb3e3de9
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-06 18:00:26 +0530

    celsius to fahrenheit

---
+++ Programiz100/CelsiustoFahrenheit.js
@@ -0,0 +1,9 @@
+// celsius to fahrenheit
+
+const celsius = 20;
+
+// celsius to fahrenheit 
+// formula celsius multiply by 1.8 or (9/5) plus 32
+const result = celsius * 1.8 + 32;
+
+console.log(result);
