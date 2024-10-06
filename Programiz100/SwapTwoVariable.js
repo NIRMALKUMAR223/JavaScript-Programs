6069e3f38bb6285214063f736f5b129f1185c3a4
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-06 14:29:34 +0530

    swap to variables

---
+++ Programiz100/SwapTwoVariable.js
@@ -0,0 +1,14 @@
+// swap to variables
+
+var number1 = 20;
+var number2 = 30;
+
+console.log('Before swap');
+console.log(number1 +" : " + number2);
+
+var temp = number1;
+number1 = number2;
+number2 = temp;
+
+console.log('After swap');
+console.log(number1 +" : " + number2);
