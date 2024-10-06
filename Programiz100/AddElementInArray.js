792d1a32cb58f743d39fdc670b56ff066bfec900
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-06 22:34:19 +0530

    add element in an array

---
+++ Programiz100/AddElementInArray.js
@@ -0,0 +1,9 @@
+// JavaScript Program to Insert Item in an Array
+
+const arr = [1,2,4,5];
+// arr.push(10);
+// arr.push('hello');
+
+// specific index  and how many element we want to remove and which string you want to add 
+arr.splice( 2,0,'Omg');  
+console.log(arr);
