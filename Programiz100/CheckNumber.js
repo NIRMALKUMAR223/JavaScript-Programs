82114ad40d00cb7c076210c09d861ae459add731
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-06 18:09:01 +0530

    positive negative zero

---
+++ Programiz100/CheckNumber.js
@@ -0,0 +1,21 @@
+// check a given number postive or negitive or zero
+
+const number = 0;
+
+// using ternary operator
+console.log( 
+    ( number < 0)?
+    'Negative Number' : ( number > 0 )?
+    'Postive Number' : 'Zero'
+);
+
+// using if - else
+// if ( number < 0 ){
+//     console.log("Negative Number");
+// }
+// else if ( number > 0 ){
+//     console.log("Positive Number")
+// }
+// else{
+//     console.log("Zero");
+// }
