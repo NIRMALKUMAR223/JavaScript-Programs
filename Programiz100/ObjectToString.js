04c1fa616d31aef319844858b12b4a1524fe0d69
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-07 14:41:07 +0530

    object to string

---
+++ Programiz100/ObjectToString.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Convert Objects to Strings
+
+const obj = {
+    firstName : 'Nirmal',
+    lastName : 'Kumar'
+}
+
+// Takes a JavaScript object as input.
+var str = JSON.stringify(obj);
+// Returns a JSON-formatted string representing the object.
+
+
+console.log(str);
