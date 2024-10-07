24673846cfe17465e0f4854649fc917d32efb5d3
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-07 14:02:21 +0530

    ascii value of an char

---
+++ Programiz100/AsciiValueOfChar.js
@@ -0,0 +1,6 @@
+// JavaScript Program to Find ASCII Value of Character
+
+const char = 'a';
+
+// char code is identify by charCodeAt() it will return the ascii value of an char
+console.log(char.charCodeAt());
