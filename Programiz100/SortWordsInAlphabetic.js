314fc114b7ba4d74095aa3f76780733828ecb0fe
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-07 14:10:37 +0530

    sort words in alpahabetic

---
+++ Programiz100/SortWordsInAlphabetic.js
@@ -0,0 +1,6 @@
+// JavaScript Program to Sort Words in Alphabetical Order
+
+const str = 'welcome to the world of javascript';
+
+// split(' ') is split the word based on the space and sort() is used to sort the word
+console.log(str.split(' ').sort());
