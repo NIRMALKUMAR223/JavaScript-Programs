ba7c59c878c1f8f7102ce9dfd5697b40e6c8890d
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-07 14:24:08 +0530

    key value to object

---
+++ Programiz100/AddKeyValueToObject.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Add Key/Value Pair to an Object
+
+const obj = {
+    firstName : 'Nirmal',
+    lastName : 'Kumar'
+}
+
+// two ways we can add elements to object 
+// ------>  obj['key'] = value;
+// ------>  obj.key = value;
+obj['age'] = 23;
+obj.DoB = '20/10/2001';
+
+console.log(obj);
+
