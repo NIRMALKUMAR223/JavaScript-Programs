fbd1804c12dcc9ce613f947d01ad6716c10ddbd8
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-09 23:11:41 +0530

    object to an array

---
+++ Programiz100/ObjectToArray.js
@@ -0,0 +1,24 @@
+// JavaScript Program to Append an Object to an Array
+
+const arr  =[];
+
+const obj1 = {
+    name : 'Nirmal',
+    age : 22,
+}
+
+const obj2 = {
+    name : 'Kumar',
+    age : 22,
+}
+
+const obj3 = {
+    name : 'Rock',
+    age : 22,
+}
+
+// add an object to an array by using inbuild method push()
+// it is used to push any element to an array
+arr.push(obj1,obj2,obj3);
+
+console.log(arr);
