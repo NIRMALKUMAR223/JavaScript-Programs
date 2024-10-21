c5ef283484aa881ca34d111142250a0fde84fccd
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-21 13:26:50 +0530

    filter

--- .Filter.js
+++ .Filter.js
@@ -9,3 +9,38 @@ const evenArr = arr.filter((value, item, arr)=>{
 console.log(evenArr);
 
 // it is used to filter the existing arr to create an new array
+
+// realtime usecase
+
+const user = [
+    {
+        name : 'Nirmal',
+        age : 23
+    },
+
+    {
+        name : 'Das',
+        age : 17
+    },
+
+    {
+        name : 'Gowtham',
+        age : 23
+    }
+]
+
+const adult = user.filter((value)=>{
+    if (value.age>18 && value.age<120){
+        return value;
+    }
+})
+
+const child = user.filter((value)=>{
+    if (value.age>0 && value.age<=18){
+        return value;
+    }
+})
+
+console.log('Adult : \n', adult);
+console.log('Children : \n', child);
+
