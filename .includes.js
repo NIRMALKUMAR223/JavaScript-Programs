65b99ec3587cadd32e48d83c9c02590c2f4bdc70
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-10-31 20:17:21 +0530

    update

--- .includes.js
+++ .includes.js
@@ -4,4 +4,4 @@ const arr = [1,2,3,4,5,6,7,8,9,10];
 
 console.log(arr.includes(3));
 
-// includes is an method which is used to check the an element is present or not
+// includes is an method which is used to check the an element is present or not.
